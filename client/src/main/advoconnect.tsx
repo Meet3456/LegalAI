95c63f36f6d1bbec83abd1c40b18ca12d811879a
Meet3456 <meetvasa09@gmail.com>
2024-02-04 03:29:54 +0530

    advoconnect

--- client/src/main/advoconnect.tsx
+++ client/src/main/advoconnect.tsx
@@ -72,6 +72,84 @@ const LawyerCard: React.FC<LawyerCardProps> = ({
     }
   };
 
+  const displayPopup = () => {
+    console.log("inside popup function");
+    return (
+      <div>
+        <div
+          id='static-modal'
+          data-modal-backdrop='static'
+          // tabIndex='-1'
+          aria-hidden='true'
+          className='hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full'
+        >
+          <div className='relative p-4 w-full max-w-2xl max-h-full'>
+            <div className='relative bg-white rounded-lg shadow dark:bg-gray-700'>
+              <div className='flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600'>
+                <h3 className='text-xl font-semibold text-gray-900 dark:text-white'>
+                  Static modal
+                </h3>
+                <button
+                  type='button'
+                  className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white'
+                  data-modal-hide='static-modal'
+                >
+                  <svg
+                    className='w-3 h-3'
+                    aria-hidden='true'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 14 14'
+                  >
+                    <path
+                      stroke='currentColor'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                      stroke-width='2'
+                      d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
+                    />
+                  </svg>
+                  <span className='sr-only'>Close modal</span>
+                </button>
+              </div>
+              <div className='p-4 md:p-5 space-y-4'>
+                <p className='text-base leading-relaxed text-gray-500 dark:text-gray-400'>
+                  With less than a month to go before the European Union enacts
+                  new consumer privacy laws for its citizens, companies around
+                  the world are updating their terms of service agreements to
+                  comply.
+                </p>
+                <p className='text-base leading-relaxed text-gray-500 dark:text-gray-400'>
+                  The European Union’s General Data Protection Regulation
+                  (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
+                  common set of data rights in the European Union. It requires
+                  organizations to notify users as soon as possible of high-risk
+                  data breaches that could personally affect them.
+                </p>
+              </div>
+              <div className='flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600'>
+                <button
+                  data-modal-hide='static-modal'
+                  type='button'
+                  className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                >
+                  I accept
+                </button>
+                <button
+                  data-modal-hide='static-modal'
+                  type='button'
+                  className='ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600'
+                >
+                  Decline
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   // ------------------------------------------------------------------
 
   return (
@@ -90,8 +168,8 @@ const LawyerCard: React.FC<LawyerCardProps> = ({
           </div>
           <div className='mt-1'>Practice Areas: {specialization}</div>
           <div className='mt-1'>Minimun Cost per Hour: {min_cost_per_hr}</div>
-          <div className='mt-1' style={{ minHeight: '2em' }}>
-          Languages: {languages}
+          <div className='mt-1' style={{ minHeight: "2em" }}>
+            Languages: {languages}
           </div>
           <div className='mt-2'>
             <span className='text-md font-semibold'>
@@ -155,7 +233,22 @@ const LawyerCard: React.FC<LawyerCardProps> = ({
                   className='bg-gray-100'
                   placeholder='Enter Description'
                 ></textarea>
-                <button type='submit' className='btn'>
+                {/* <button
+                  onClick={displayPopup}
+                  data-modal-target='static-modal'
+                  data-modal-toggle='static-modal'
+                  className='btn block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                  type='submit'
+                >
+                  Get Connected
+                </button> */}
+                <button
+                  // onClick={window.alert(
+                  //   "Successfully Connected to the Lawyer!"
+                  // )}
+                  type='submit'
+                  className='btn'
+                >
                   Get Connected
                 </button>
                 <button
@@ -195,15 +288,13 @@ const Input: React.FC<InputProps> = ({ placeholder, onChange }) => (
   />
 );
 
-
-
 const Lawyers: React.FC = () => {
   const [lawyersData, setLawyersData] = useState<Lawyer[]>([]);
   const [filteredLawyers, setFilteredLawyers] = useState<Lawyer[]>([]);
   const [searchInput, setSearchInput] = useState<string>("");
 
   const getLawyersDataFromServer = async (searchValue: string) => {
-    console.log("input value",searchValue)
+    console.log("input value", searchValue);
     try {
       const response = await axios.post("/api/user/get-advocate", {
         search: searchValue,
@@ -223,8 +314,8 @@ const Lawyers: React.FC = () => {
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(event.target.value)
-  }
+    setSearchInput(event.target.value);
+  };
 
   const showLawyerCards = () => {
     if (lawyersData.length === 0) {
@@ -233,14 +324,16 @@ const Lawyers: React.FC = () => {
 
     return (
       <>
-      <div className="ml-5"><h2>Recommendation on Location & Minimum Cost</h2></div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        {lawyersData.map((lawyer) => (
-          <div key={lawyer.advocate_id} className='mb-4 mr-5'>
-            <LawyerCard {...lawyer} />
-          </div>
-        ))}
-      </div>
+        <div className='ml-5'>
+          <h2>Recommendation on Location & Minimum Cost</h2>
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {lawyersData.map((lawyer) => (
+            <div key={lawyer.advocate_id} className='mb-4 mr-5'>
+              <LawyerCard {...lawyer} />
+            </div>
+          ))}
+        </div>
       </>
     );
   };
@@ -297,10 +390,10 @@ const Lawyers: React.FC = () => {
         onChange={(e) => searchItems(e.target.value)}
       /> */}
         <div className='mb-6'>
-          <label className='block mb-2 text-4xl font-medium text-white dark:text-white ml-6 underline' >
+          <label className='block mb-2 text-4xl font-medium text-black dark:text-white ml-6 underline'>
             Connect to Advocate:
           </label>
-          <div className="input"style={{ display: "flex" }}>
+          <div className='input' style={{ display: "flex" }}>
             <input
               type='text'
               id='large-input'
